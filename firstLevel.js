f18596637e6c5592279966ee4c96daac8a60b5dc
joshroby <notify@joshroby.com>
2017-03-17 22:57:45 -0500

    Widened First Level

--- firstLevel.js
+++ firstLevel.js
@@ -1,6 +1,6 @@
 var firstLevel = {
  
- 	sizeX: 10,
+ 	sizeX: 12,
  	sizeY: 10,
  	
  	background: 'img/background.png',
@@ -11,74 +11,81 @@ var firstLevel = {
  	],
  	
  	walls: [
- 		{x:0,y:4},
- 		{x:7,y:0},
+ 		{x:1,y:4},
  		{x:8,y:0},
  		{x:9,y:0},
- 		{x:8,y:1},
+ 		{x:10,y:0},
  		{x:9,y:1},
- 		{x:7,y:2},
- 		{x:6,y:3},
+ 		{x:10,y:1},
+ 		{x:8,y:2},
  		{x:7,y:3},
- 		{x:9,y:3},
- 		{x:7,y:4},
- 		{x:8,y:5},
- 		{x:9,y:6},
- 		{x:9,y:7},
- 		{x:8,y:8},
+ 		{x:8,y:3},
+ 		{x:10,y:3},
+ 		{x:8,y:4},
+ 		{x:9,y:5},
+ 		{x:10,y:6},
+ 		{x:10,y:7},
  		{x:9,y:8},
- 		{x:8,y:9},
+ 		{x:10,y:8},
  		{x:9,y:9},
- 		{x:5,y:5},
- 		{x:5,y:6},
- 		{x:6,y:7},
- 		{x:5,y:8},
- 		{x:5,y:9},
+ 		{x:10,y:9},
+ 		{x:6,y:5},
+ 		{x:6,y:6},
+ 		{x:7,y:7},
+ 		{x:6,y:8},
+ 		{x:6,y:9},
+ 		{x:11,y:0},
+ 		{x:11,y:1},
+ 		{x:11,y:2},
+ 		{x:11,y:3},
+ 		{x:11,y:4},
+ 		{x:11,y:5},
+ 		{x:11,y:6},
+ 		{x:11,y:7},
+ 		{x:11,y:8},
+ 		{x:11,y:9},
  	],
  		
  	pits: [
- 		{x:2,y:0},
  		{x:3,y:0},
  		{x:4,y:0},
  		{x:5,y:0},
- 		{x:3,y:1},
+ 		{x:6,y:0},
  		{x:4,y:1},
  		{x:5,y:1},
- 		{x:3,y:2},
- 		{x:3,y:3},
- 		{x:2,y:5},
+ 		{x:6,y:1},
+ 		{x:4,y:2},
+ 		{x:4,y:3},
  		{x:3,y:5},
- 		{x:0,y:6},
+ 		{x:4,y:5},
  		{x:1,y:6},
  		{x:2,y:6},
  		{x:3,y:6},
- 		{x:0,y:7},
+ 		{x:4,y:6},
  		{x:1,y:7},
  		{x:2,y:7},
  		{x:3,y:7},
  		{x:4,y:7},
- 		{x:0,y:8},
+ 		{x:5,y:7},
  		{x:1,y:8},
  		{x:2,y:8},
  		{x:3,y:8},
- 		{x:0,y:9},
+ 		{x:4,y:8},
  		{x:1,y:9},
  		{x:2,y:9},
  		{x:3,y:9},
+ 		{x:4,y:9},
+ 		{x:0,y:6},
+ 		{x:0,y:7},
+ 		{x:0,y:8},
+ 		{x:0,y:9},
  	],
  	
  	mobs: [
  		{
-			x:9,
+			x:10,
 			y:2,
 			id:dataMobs.hellhound,
-			ai: 'dormant',
- 		},
- 		
- 		{
-			x:6,
-			y:9,
-			id:dataMobs.rat,
 			ai: 'dormant',
  		},
  		
@@ -88,29 +95,36 @@ var firstLevel = {
 			id:dataMobs.rat,
 			ai: 'dormant',
  		},
+ 		
+ 		{
+			x:8,
+			y:9,
+			id:dataMobs.rat,
+			ai: 'dormant',
+ 		},
  	],
  	
  	triggers: [
  		{
- 			x: 2,
+ 			x: 3,
  			y: 4,
  			event: 'intro',
  		},
  		
  		{
- 			x: 9,
+ 			x: 10,
  			y: 5,
  			event: 'noHellhound',
  		},
  		
  		{
- 			x: 8,
+ 			x: 9,
  			y: 2,
  			event: 'puppy',
  		},
  		
  		{
- 			x: 5,
+ 			x: 6,
  			y: 4,
  			event: 'rats',
  		},
@@ -138,9 +152,9 @@ var firstLevel = {
  			view.displayDialogue("Ugh, I see rat droppings.  Is this cave filled with giant rats?  What is this, the first level of a fantasy RPG?",this.name,this.img,"left");
  			this.location.event = undefined;
  			mobs[4].stats.move = 4;
- 			mobs[4].move(map.hexes[67]);
+ 			mobs[4].move(map.hexes[104]);
  			mobs[3].stats.move = 4;
- 			mobs[3].move(map.hexes[77]);
+ 			mobs[3].move(map.hexes[92]);
  			mobs[3].ai = ai.basic;
  			mobs[4].ai = ai.basic;
  		},
